572d0a70e1dbe168d9f6acd02c1835acb9aa0788
Wesam Haboush <wesam.haboush@gmail.com>
2016-05-08 22:37:16 -0700

    tidy up

--- algorithms/src/test/java/com/codebreeze/algorithms/DistinctPowersTest.java
+++ algorithms/src/test/java/com/codebreeze/algorithms/DistinctPowersTest.java
@@ -10,20 +10,20 @@ public class DistinctPowersTest
     @Test
     public void calculate() throws Exception
     {
-//        System.out.println(DistinctPowers.calculateStoreRoots(2, 11));
-//        System.out.println(DistinctPowers.calculateStoreRoots(2, 16)); //20^20
-        assertThat(DistinctPowers.calculateStoreRoots(2, 5)).isEqualTo(15);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 6)).isEqualTo(23);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 8)).isEqualTo(44);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 9)).isEqualTo(54);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 10)).isEqualTo(69);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 11)).isEqualTo(88);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 12)).isEqualTo(106);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 15)).isEqualTo(177);
-        assertThat(DistinctPowers.calculateStoreRoots(2, 16)).isEqualTo(195);
+//        System.out.println(DistinctPowers.calculateStoreReduced(2, 11));
+//        System.out.println(DistinctPowers.calculateStoreReduced(2, 16)); //20^20
+        assertThat(DistinctPowers.calculateStoreReduced(2, 5)).isEqualTo(15);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 6)).isEqualTo(23);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 8)).isEqualTo(44);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 9)).isEqualTo(54);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 10)).isEqualTo(69);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 11)).isEqualTo(88);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 12)).isEqualTo(106);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 15)).isEqualTo(177);
+        assertThat(DistinctPowers.calculateStoreReduced(2, 16)).isEqualTo(195);
 //        assertThat(DistinctPowers.calculate6(2, 20)).isEqualTo(320);
         //                assertThat(DistinctPowers.calculate5(2, 100)).isEqualTo(9182L);//9183
-        assertThat(DistinctPowers.calculateStoreRoots(2, 100)).isEqualTo(9183);//9183
+        assertThat(DistinctPowers.calculateStoreReduced(2, 100)).isEqualTo(9183);//9183
     }
 
     @Test
@@ -43,5 +43,4 @@ public class DistinctPowersTest
         //                assertThat(DistinctPowers.calculate5(2, 100)).isEqualTo(9182L);//9183
         assertThat(DistinctPowers.calculateCounting(2, 100)).isEqualTo(9183);//9183
     }
-
 }
